8be6855225da5c523f28a3e31647caa2d6c22fdb
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-05-16 22:38:46 -0400

    app.js initial set up with pulling movie information from API

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import MovieContainer from './MovieContainer'
 
+const API_URL =
+  'https://api.themoviedb.org/3/movie/popular?api_key=12351e0a6e986dffd037f9e5d2d739d3'
 function App() {
-  return <div>
-    Movie App
-  </div>
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setMovies(data.results)
+      })
+  }, [])
+
+  return (
+    <div>
+      {movies.map((movieReq) => (
+        <MovieContainer key={movieReq.id} {...movieReq} />
+      ))}
+    </div>
+  )
 }
 
 export default App
