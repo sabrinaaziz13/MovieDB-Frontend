f1b349d2ec440e9798dbe1c4f97861e3ed8db062
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-05-16 22:55:02 -0400

    added additional parameters

--- movie-app/src/MovieContainer.js
+++ movie-app/src/MovieContainer.js
@@ -1,14 +1,24 @@
-import React from "react";
-const API_IMG="https://image.tmdb.org/t/p/w500/";
+import React from 'react'
+const API_IMG = 'https://image.tmdb.org/t/p/w500/'
 
-const MovieContainer=({title, poster_path, vote_average, release_date, overview})=>{
-  return(
-    <div>
-    <h1>{title}</h1>
-    <img src={API_IMG+poster_path}></img>
-    <p>{overview}</p>
-    </div>
+const MovieContainer = ({
+  title,
+  poster_path,
+  vote_average,
+  release_date,
+  overview
+}) => {
+  return (
+    <main>
+      <div>
+        <h1>{title}</h1>
+        <img src={API_IMG + poster_path}></img>
+        <h3>Rating: {vote_average}</h3>
+        <h3>Release Date: {release_date}</h3>
+        <p>{overview}</p>
+      </div>
+    </main>
   )
 }
 
-export default MovieContainer;
+export default MovieContainer
