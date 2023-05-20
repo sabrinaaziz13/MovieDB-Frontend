5ce9674b8d20ced0b0da19895e3dd292e031841a
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-05-20 14:02:00 -0400

    added overview header

--- movie-app/src/MovieContainer.js
+++ movie-app/src/MovieContainer.js
@@ -1,6 +1,6 @@
-import { Modal, show, Button } from 'react-bootstrap';
-import React, {useState} from 'react';
-const API_IMG = 'https://image.tmdb.org/t/p/w500/';
+import { Modal, show, Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+const API_IMG = 'https://image.tmdb.org/t/p/w500/'
 
 const MovieContainer = ({
   title,
@@ -9,33 +9,36 @@ const MovieContainer = ({
   release_date,
   overview
 }) => {
+  const [show, setShow] = useState(false)
 
-  const [show, setShow]=useState(false);
+  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false)
 
-  const handleShow=()=>setShow(true);
-  const handleClose=()=>setShow(false);
-
-  
   return (
     <div className="card text-center bg-secondary mb-3">
       <div className="card-body">
         <img className="card-img-top" src={API_IMG + poster_path} />
         <div className="card-body">
-          <button type="button" className="btn btn-dark" onClick={handleShow}>View More</button>
+          <button type="button" className="btn btn-dark" onClick={handleShow}>
+            View More
+          </button>
           <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-            <Modal.Title></Modal.Title>
+              <Modal.Title></Modal.Title>
             </Modal.Header>
-            <Modal.Body >
+            <Modal.Body>
               <img className="card-img-top" src={API_IMG + poster_path} />
               <h3>{title}</h3>
               <h4>Rating: {vote_average}</h4>
               <h5>Release Date: {release_date}</h5>
               <br></br>
+              <h6>Overview</h6>
               <p>{overview}</p>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>Close</Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
             </Modal.Footer>
           </Modal>
         </div>
