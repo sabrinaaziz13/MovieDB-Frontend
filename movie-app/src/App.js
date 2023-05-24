b2155f233c54f1b25c31ca3d0d0d8aa92d082402
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-05-23 22:53:02 -0400

    movie db front end still working

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import MovieContainer from './MovieContainer'
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button
+} from 'react-bootstrap'
 // import 'bootstrap/dist/css/bootsrap.min.css'
 
 const API_URL =
   'https://api.themoviedb.org/3/movie/popular?api_key=12351e0a6e986dffd037f9e5d2d739d3'
+const API_SEARCH =
+  'https://api.themoviedb.org/3/search/movie?api_key=12351e0a6e986dffd037f9e5d2d739d3&query'
+
 function App() {
   const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     fetch(API_URL)
@@ -17,14 +29,67 @@ function App() {
       })
   }, [])
 
+  const searchMovie = async (e) => {
+    e.preventDefault()
+    console.log('Searching')
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=12351e0a6e986dffd037f9e5d2d739d3&query=${query}`
+      const res = await fetch(url)
+      const data = await res.json()
+      console.log(data)
+      setMovies(data.results)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value)
+  }
   return (
-    <div className="container">
-      <div className="grid">
-        {movies.map((movieReq) => (
-          <MovieContainer key={movieReq.id} {...movieReq} />
-        ))}
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/home">Movie Db App</Navbar.Brand>
+          <Navbar.Brand href="/home">Trending</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="nbar"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            ></Nav>
+
+            <Form className="form" onSubmit={searchMovie}></Form>
+            <FormControl
+              type="search"
+              placeholder="Search Movies"
+              className="searchbar"
+              aria-label="search"
+              name="query"
+              value={query}
+              onChange={changeHandler}
+            ></FormControl>
+            <Button variant="secondary" type="submit">
+              Search
+            </Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+        {movies.length > 0 ? (
+          <div className="container">
+            <div className="grid">
+              {movies.map((movieReq) => (
+                <MovieContainer key={movieReq.id} {...movieReq} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <h2>Movie not found</h2>
+        )}
       </div>
-    </div>
+    </>
   )
 }
 
--- movie-app/src/MovieContainer.js
+++ movie-app/src/MovieContainer.js
@@ -16,35 +16,35 @@ const MovieContainer = ({
 
   return (
     <main>
-    <div className="card text-center bg-secondary mb-3">
-      <div className="card-body">
-        <img className="card-img-top" src={API_IMG + poster_path} />
+      <div className="card text-center bg-secondary mb-3">
         <div className="card-body">
-          <button type="button" className="btn btn-dark" onClick={handleShow}>
-            View More
-          </button>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title></Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <img className="card-img-top" src={API_IMG + poster_path} />
-              <h3>{title}</h3>
-              <h4>Rating: {vote_average}</h4>
-              <h5>Release Date: {release_date}</h5>
-              <br></br>
-              <h6>Overview</h6>
-              <p>{overview}</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
+          <img className="card-img-top" style={{width:'28rem'}}src={API_IMG + poster_path} />
+          <div className="card-body">
+            <button type="button" className="btn btn-dark" onClick={handleShow}>
+              View More
+            </button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title></Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <img className="card-img-top" src={API_IMG + poster_path} />
+                <h3>{title}</h3>
+                <h4>Rating: {vote_average}</h4>
+                <h5>Release Date: {release_date}</h5>
+                <br></br>
+                <h6>Overview</h6>
+                <p>{overview}</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
       </div>
-    </div>
     </main>
   )
 }
